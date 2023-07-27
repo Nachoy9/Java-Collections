5a6fc2b2d35a7b01a5b85c73ea07c46ae45cd604
Ignacio Cholaquides <nachoy9@hotmail.com>
2023-07-27 20:34:07 -0300

    Java Colecciones Ejercicios Extra (4)

--- src/EjercicioExtra4/Principal/Principal.java
+++ src/EjercicioExtra4/Principal/Principal.java
@@ -1,2 +1,76 @@
-package EjercicioExtra4.Principal;public class Principal {
+/*
+Almacena en un HashMap los códigos postales de 10 ciudades a elección de esta página: https://mapanet.eu/index.htm.
+Nota: Poner el código postal sin la letra, solo el número.
+• Pedirle al usuario que ingrese 10 códigos postales y sus ciudades.
+• Muestra por pantalla los datos introducidos
+• Realizar un menu que permita, agregar otra ciudad, buscar por codigo postal, mostrar todos los datos y eliminar ciudad.
+*/
+
+package EjercicioExtra4.Principal;
+
+import java.util.Scanner;
+import EjercicioExtra4.Servicios.CPServicio;
+
+public class Principal {
+
+    private static CPServicio CPSer = new CPServicio();
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int opc;
+        boolean salir = false;
+
+        iniciarPrograma();
+
+        while (!salir) {
+
+            System.out.println("\nMenu principal\n");
+            System.out.println("1. Añadir ciudad.");
+            System.out.println("2. Eliminar ciudad.");
+            System.out.println("3. Mostrar ciudades.");
+            System.out.println("4. Buscar ciudad.");
+            System.out.println("5. Salir");
+            System.out.println("\nIngrese opcion:");
+
+            opc = input.nextInt();
+            input.nextLine();
+
+            switch (opc) {
+                case 1:
+                    CPSer.ingresarCiudad();
+                    break;
+                case 2:
+                    CPSer.eliminarCiudad();
+                    break;
+                case 3:
+                    System.out.println("\nListado de ciudades ingresadas:");
+                    CPSer.mostrarCiudades();
+                    break;
+                case 4:
+                    CPSer.buscarPorCP();
+                    break;
+                case 5:
+                    System.out.println("\nCerrando aplicacion, hasta luego.");
+                    salir = true;
+                    break;
+                default:
+                    System.out.println("Opcion incorrecta, intente nuevamente.");
+                    break;
+            }
+        }
+    }
+
+    public static void iniciarPrograma() {
+
+        System.out.println("Bienvenido");
+
+        for (int i = 0; i < 10; i++) {
+
+            System.out.println("\nCiudad Nro " + (i+1));
+            CPSer.ingresarCiudad();
+
+        }
+    }
+
 }
--- src/EjercicioExtra4/Servicios/CPServicio.java
+++ src/EjercicioExtra4/Servicios/CPServicio.java
@@ -1,2 +1,80 @@
-package EjercicioExtra4.Servicios;public class CPServicio {
+package EjercicioExtra4.Servicios;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class CPServicio {
+
+    private HashMap<Integer, String> cps = new HashMap<>();
+    private Scanner input = new Scanner(System.in);
+
+    public void ingresarCiudad() {
+
+        int cp;
+        String ciudad;
+
+        System.out.println("Ingrese el nombre de la ciudad:");
+        ciudad = input.nextLine().toUpperCase();
+
+        System.out.println("Ingrese el codigo postal de " + ciudad + ":");
+        cp = input.nextInt();
+        input.nextLine();
+
+        cps.put(cp,ciudad);
+
+    }
+
+    public void mostrarCiudades() {
+
+        TreeMap<Integer, String> ordenadosPorCP = new TreeMap<>(cps);
+
+        int contador = 0;
+
+        for (Map.Entry<Integer, String> ciudad : ordenadosPorCP.entrySet()) {
+            contador++;
+            System.out.println(contador + ". CP: " + ciudad.getKey() + " -  " + ciudad.getValue());
+        }
+
+    }
+
+    public void buscarPorCP() {
+
+        int cp;
+
+        System.out.println("Ingrese codigo postal:");
+        cp = input.nextInt();
+        input.nextLine();
+
+        if (existeCiudad(cp)) {
+            System.out.println("El codigo " + cp + " corresponde a la ciudad " + cps.get(cp));
+        } else {
+            System.out.println("No existe ciudad para el Codigo Postal ingresado.");
+        }
+
+    }
+
+    public boolean existeCiudad(int cp) {
+        return cps.containsKey(cp);
+    }
+
+    public void eliminarCiudad() {
+
+        int cp;
+
+        System.out.println("Ingrese codigo postal:");
+        cp = input.nextInt();
+        input.nextLine();
+
+        if (existeCiudad(cp)) {
+            System.out.println("Se ha eliminaod " + cps.get(cp) + " del listado.");
+            cps.remove(cp);
+        } else {
+            System.out.println("No existe ciudad para el Codigo Postal ingresado.");
+        }
+
+    }
+
+
 }
